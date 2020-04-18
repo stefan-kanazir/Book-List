a60c7525df3fbbd20fbfb43abb1e4a0155796141
Stefan Kanazir <kanazir.stefan@gmail.com>
2020-04-18 20:12:43 +0200

    commit on 'master'

---
+++ app.js
@@ -0,0 +1,87 @@
+// Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add Book to List
+UI.prototype.addBookToList = function(book) {
+    const list = document.getElementById('book-list');
+
+    // Create tr Element
+    const row = document.createElement('tr');
+
+    // Insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href = "" class = "delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+
+    console.log(row);
+}
+
+// Show Alert
+UI.prototype.showAllert = function(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+
+    // Insert Alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3s
+    setTimeout(function() {
+        document.querySelector('.alert').remove()
+    }, 3000);
+}
+
+// Clear Fields
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+// Event Listeneres
+document.getElementById('book-form').addEventListener('submit', function(e) {
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+    
+    // Instantiate Book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI 
+    const ui = new UI();
+
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+        // Error Alert
+        ui.showAllert('Please fill in all fields', 'error');
+    } else {
+        // Add Book to list
+        ui.addBookToList(book);
+
+        // Show Success
+        ui.showAllert('Book Added!', 'success');
+
+        // Clear fields
+        ui.clearFields();
+    }
+
+    e.preventDefault();
+});
